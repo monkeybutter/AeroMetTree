9460e12aee134763aee780e4797c88d146acf787
monkeybutter <p.rozas.larraondo@gmail.com>
2017-02-11 14:36:14 +1100

    Data extractor added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/Users/pablo/anaconda/envs/python3/bin/python
+#!/usr/bin/python
 
 import json
 import pickle
@@ -40,7 +40,7 @@ def tree_pprinter(node):
     return pprinter(node)
 
 
-def tree_trainer(df, class_var, var_desc, stop=50, variance=.01):
+def tree_trainer(df, class_var, var_desc, stop=50, variance=.05):
         if class_var not in df.columns:
             raise Exception('Class variable not in DataFrame')
 
@@ -51,26 +51,89 @@ def tree_trainer(df, class_var, var_desc, stop=50, variance=.01):
 
         return node
 
+
+def is_in_bounds(bounds, value):
+    for bound in bounds:
+        if bound[0] > bound[1]:
+            if bound[0] <= value <= 360.0 or 0.0 <= value < bound[1]:
+                return True
+        elif bound[0] == 0.0 and value == 360:
+            return True
+        else:
+            if bound[0] <= value < bound[1]:
+                return True
+
+    return False
+
+
+def tree_eval(node, row):
+    result = None
+
+    def eval(node, row):
+        nonlocal result
+
+        if node.split_var is None:
+            result = np.mean(node.data.df[node.data.class_var].values)
+
+        else:
+            if is_in_bounds(node.left_child.data.var_desc[node.split_var]["bounds"], row[node.split_var]):
+                eval(node.left_child, row)
+            elif is_in_bounds(node.right_child.data.var_desc[node.split_var]["bounds"], row[node.split_var]):
+                eval(node.right_child, row)
+            else:
+                print(node.data.var_desc[node.split_var]["bounds"], row[node.split_var])
+                print(node.left_child.data.var_desc[node.split_var]["bounds"], row[node.split_var])
+                print(node.right_child.data.var_desc[node.split_var]["bounds"], row[node.split_var])
+                sys.exit("tree_eval() problem with bounds ????")
+
+    eval(node, row)
+
+    return result
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='AeroMetTree model training command line utility.')
+    parser.add_argument('--data', dest='data_path', help='Path to the CSV file containing the data to train the model')
+    parser.add_argument('--config', dest='config_path', help='Path to the CSV file containing the data to train the model')
 
-    df = pd.read_csv("./datasets/egll_clean.csv")
+    args = parser.parse_args()
+    #print(args.data_path)
+
+    df = pd.read_csv(args.data_path)
     tree = None
+    model_name = os.path.splitext(os.path.basename(args.config_path))[0] + ".save"
 
-    if os.path.isfile("model.save"):
-        tree = pickle.load(open("model.save", "rb"))
+    """
+    if os.path.isfile(model_name):
+        tree = pickle.load(open(model_name, "rb"))
         tree_pprinter(tree)
         exit(1)
+    """
 
-    with open('input.json') as conf_file:    
+    with open(args.config_path) as conf_file:    
         tree_params = json.load(conf_file)
-        print(tree_params)
+        #print(tree_params)
 
         class_var = tree_params['output']
         # 1 Classic
         tree_desc = {}
         for var in tree_params['input']:
             tree_desc[var['name']] = {"type": var['type'], "method": "classic", "bounds": [[-np.inf, np.inf]]}
-    print(tree_desc)
-    tree = tree_trainer(df, class_var, tree_desc)
-    tree_pprinter(tree)
-    pickle.dump(tree, open("model.save", "wb"))
+   
+    for size in [1000, 500, 250, 100, 50]:
+        #print(tree_desc)
+        #tree = tree_trainer(df, class_var, tree_desc, tree_params['max_leaf_size'])
+        tree = tree_trainer(df, class_var, tree_desc, size)
+        #tree_pprinter(tree)
+        pickle.dump(tree, open(model_name, "wb"))
+
+        acc_tree = 0.0
+        acc_gfs = 0.0
+        for _, row in df.iterrows():
+            acc_tree += (row[tree.data.class_var] - tree_eval(tree, row)) ** 2 
+            acc_gfs += (row[tree.data.class_var] - row["gfs_wind_spd"]) ** 2 
+       
+        print("max_leaf_size", size)
+        print("Abs Tree error:", (acc_tree/len(df.index)) ** .5)
+        print("Abs GFS error", (acc_gfs/len(df.index)) ** .5)
+        print("___________")
